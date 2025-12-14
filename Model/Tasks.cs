28a6413b26d388f60774f4c7a62bae3acd0aea77
Alex-dev1356 <asoliven26@yahoo.com>
2025-12-15 07:35:53 +0800

    Creating Model for Tasks

---
+++ Model/Tasks.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagerAPI.Model
+{
+    public class Tasks
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public bool IsCompleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
