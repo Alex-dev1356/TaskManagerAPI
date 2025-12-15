35dfb955d778ae7e238a2ca402e2686a12332328
Alex-dev1356 <asoliven26@yahoo.com>
2025-12-15 08:28:27 +0800

    Creating Data Folder for the Declaration of our DBContext Utilization

---
+++ Data/TaskManagerAPIContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerAPI.Model;
+
+namespace TaskManagerAPI.Data
+{
+    public class TaskManagerAPIContext : DbContext
+    {
+        public TaskManagerAPIContext(DbContextOptions<TaskManagerAPIContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Tasks> Tasks { get; set; } = null!;
+    }
+}
