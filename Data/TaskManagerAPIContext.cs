db4a1f8f43c7b0a8cd7110dfec1dd0d4e14a139c
Alex-dev1356 <asoliven26@yahoo.com>
2025-12-15 12:02:30 +0800

    Adding Data to the Table by Overriding the OnModelCreating Method for the purpose of Asynchronous Programming

--- Data/TaskManagerAPIContext.cs
+++ Data/TaskManagerAPIContext.cs
@@ -8,9 +8,40 @@ namespace TaskManagerAPI.Data
         public TaskManagerAPIContext(DbContextOptions<TaskManagerAPIContext> options)
             : base(options)
         {
-            
+
         }
 
         public DbSet<Tasks> Tasks { get; set; } = null!;
+
+        //Inserting Data to our Database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tasks>().HasData(
+                new Tasks
+                {
+                    ID = 1,
+                    Title = "Task 1",
+                    Description = "Description for Task 1",
+                    IsCompleted = false,
+                    CreatedAt = new DateTime(2024, 1, 1)
+                },
+                new Tasks
+                {
+                    ID = 2,
+                    Title = "Task 2",
+                    Description = "Description for Task 2",
+                    IsCompleted = false,
+                    CreatedAt = new DateTime(2024, 12, 1)
+                },
+                new Tasks
+                {
+                    ID = 3,
+                    Title = "Task 3",
+                    Description = "Description for Task 3",
+                    IsCompleted = false,
+                    CreatedAt = new DateTime(2024, 3, 1)
+                }
+            );
+        }
     }
 }
